f15c58d504fd9acc2317e66721d14cb8c5263a2a
Catalin Lazar <lazcatluc@gmail.com>
2015-03-20 12:03:01 +0200

    add application name as parameter in scripts

--- jmeter/runJmeterTests.sh
+++ jmeter/runJmeterTests.sh
@@ -3,7 +3,7 @@ failed=0
 for test in *.jmx; do 
 	logfile=$test".log"
 	rm $logfile
-	jmeter.sh -n -t $test -l $logfile
+	jmeter.sh -JHOST=$1 -JPORT=$2 -JNAME=$3 -n -t $test -l $logfile
 	if grep -q "false" $logfile; then
 		echo "Failed: $test"
 		failed=1
