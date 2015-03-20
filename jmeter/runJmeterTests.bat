e1d339f62fbb8c1706e5eef8e8e15ca800e81826
Catalin Lazar <lazcatluc@gmail.com>
2015-03-20 11:54:32 +0200

    fix jmeter test

--- jmeter/runJmeterTests.bat
+++ jmeter/runJmeterTests.bat
@@ -2,7 +2,7 @@ for %%f in (*.jmx) do (
 	
 	del "%%~nf.jmx.log"
 	
-	jmeter.bat -n -t %%f -l "%%~nf.jmx.log"
+	jmeter.bat -JHOST=%1 -JPORT=%2 -n -t %%f -l "%%~nf.jmx.log"
 	
 	find /c "true" "%%~nf.jmx.log"
 
