11a1355d58614ca5175630a324f7e6b443cd15da
ursuandrei42 <ursuandrei42-PC>
2015-03-21 17:19:27 +0200

    refactor tests

--- src/test/java/controller/PlayerControllerTest.java
+++ src/test/java/controller/PlayerControllerTest.java
@@ -17,160 +17,128 @@ import cards.CardImpl;
 import cards.Deck;
 import cards.Rank;
 import cards.Suit;
-import de.bechte.junit.runners.context.HierarchicalContextRunner;
 import player.InvalidPlayerException;
 import player.Player;
 import player.PlayerValidatorImpl;
 
-@RunWith(HierarchicalContextRunner.class)
 public class PlayerControllerTest {
 
 	private Deck deck;
-	
+
 	private PlayerController playerController;
-	
+
 	private Card card1;
 	private Card card2;
 
 	private Table table;
-	
+
 	@Before
-	public void setup(){
+	public void setup() {
 		deck = mock(Deck.class);
 
-        card1 = new CardImpl(Rank.ACE,Suit.CLUBS);
-		card2 = new CardImpl(Rank.FIVE,Suit.HEARTS);
+		card1 = new CardImpl(Rank.ACE, Suit.CLUBS);
+		card2 = new CardImpl(Rank.FIVE, Suit.HEARTS);
 		when(deck.drawCard()).thenReturn(card1, card2);
 
-        playerController = new PlayerController();
-        
-        
-        table = new Table();
-        table.setValidator(new PlayerValidatorImpl());
+		playerController = new PlayerController();
+
+		table = new Table();
+		table.setValidator(new PlayerValidatorImpl());
 		table.setDeck(deck);
 		playerController.setTable(table);
-		
-		
-        playerController.setName("testName");
-        playerController.createPlayer();
 
-        
+		playerController.setName("testName");
+		playerController.createPlayer();
+
 	}
-	
+
 	@Test
 	public void getPlayerCardsCallsDeckTwice() {
 		playerController.getCards();
-		verify(deck,times(2)).drawCard();
+		verify(deck, times(2)).drawCard();
 	}
-	
+
 	@Test
 	public void getPlayerCardsReturnsListOfTwoCards() throws Exception {
 		List<Card> playerCards = playerController.getCards();
-		
+
 		Assertions.assertThat(playerCards).hasSize(2);
 	}
-	
+
 	@Test
 	public void getPlayerCardsReturnDifferentCards() throws Exception {
 		List<Card> firstDraw = playerController.getCards();
 		List<Card> secondDraw = playerController.getCards();
-		
+
 		Assertions.assertThat(firstDraw).isEqualTo(secondDraw);
 	}
 
-    @Test
-    public void shouldNotCreatePlayerTheSecondTime() {
-        playerController.setName("testName");
-        playerController.createPlayer();
-        Player player1 = playerController.getPlayer();
+	@Test
+	public void shouldNotCreatePlayerTheSecondTime() {
+		playerController.setName("testName");
+		playerController.createPlayer();
+		Player player1 = playerController.getPlayer();
 
-        playerController.createPlayer();
-        Player player2 = playerController.getPlayer();
+		playerController.createPlayer();
+		Player player2 = playerController.getPlayer();
 
-        assertTrue(player1 == player2);
-    }
-    
-    @Test
-    public void crashPlayerValidator() {    	
-    	PlayerController playerController = new PlayerController();
-    	playerController.setName("testName");
-    	playerController.setTable(table);
-    	
-    	System.out.println("error:"+playerController.getError());
-    	
-    	assertNull(playerController.getError());
-    	playerController.createPlayer();
-    	assertNotNull(playerController.getError());
-    	
-    	System.out.println("error:"+playerController.getError());
-    	
-    }
-    
-  public class SinglePlayerAtTable{
-    	
-    	PlayerController pc1;
-    	Table myTable;
-    	
-    	@Before
-    	public void setup() throws InvalidPlayerException{
-    		pc1 = new PlayerController();
-    		pc1.setName("Player1");
-			pc1.createPlayer();
-			
-			
-			myTable = new Table();
-			pc1.setTable(myTable);
+		assertTrue(player1 == player2);
+	}
 
-			myTable.registerPlayer(pc1.getPlayer());
-    	}
-    
-	    @Test
-		public void whenPlayerFoldsTableIsEmpty() throws Exception {
-			
-			pc1.fold();
-			
-			int numberOfPlayers = myTable.getNumberOfPlayers();
-			
-			assertEquals(0,numberOfPlayers);
-		}
-	    
-	    @Test
-		public void playerIsOwner() throws Exception {
-	    	Player player = pc1.getPlayer();
-	    	
-	    	assertTrue(myTable.isOwner(player));
-		}
-	    
-	    public class TwoPlayersAtTable {
-	    	PlayerController pc2;
-	    	
-	    	@Before
-	    	public void newPlayerJoins() throws InvalidPlayerException{
-	    		pc2 = new PlayerController();
-	    		pc2.setName("Player2");
-				pc2.createPlayer();
-				
-				pc2.setTable(myTable);
+	@Test
+	public void crashPlayerValidator() {
+		PlayerController playerController = new PlayerController();
+		playerController.setName("testName");
+		playerController.setTable(table);
 
-				myTable.registerPlayer(pc2.getPlayer());
-	    	}
-	    	
-	    	@Test
-			public void firstPlayerIsOwner() throws Exception {
-	    		Player firstPlayer = pc1.getPlayer();
-		    	
-		    	assertTrue(myTable.isOwner(firstPlayer));
-			}
-	    	
-	    	@Test
-			public void whenFirstPlayerFoldsSecondPlayerBecomesOwner() throws Exception {
-	    		pc1.fold();
-	    		
-	    		Player secondPlayer = pc2.getPlayer();
-	    		
-	    		assertTrue(myTable.isOwner(secondPlayer));
-			}
-	    }
-    }
+		System.out.println("error:" + playerController.getError());
+
+		assertNull(playerController.getError());
+		playerController.createPlayer();
+		assertNotNull(playerController.getError());
+
+		System.out.println("error:" + playerController.getError());
+
+	}
+
+	@Test
+	public void whenPlayerFoldsTableIsEmpty() throws Exception {
+
+		playerController.fold();
+
+		int numberOfPlayers = table.getNumberOfPlayers();
+
+		assertEquals(0, numberOfPlayers);
+	}
+
+	@Test
+	public void playerIsOwner() throws Exception {
+		Player player = playerController.getPlayer();
+
+		assertTrue(table.isOwner(player));
+	}
+
+	@Test
+	public void firstPlayerIsOwner() throws Exception {
+		Player firstPlayer = playerController.getPlayer();
+
+		assertTrue(table.isOwner(firstPlayer));
+	}
+
+	@Test
+	public void whenFirstPlayerFoldsSecondPlayerBecomesOwner() throws Exception {
+		PlayerController pc2 = new PlayerController();
+		pc2.setTable(table);
+
+		pc2.setName("Player2");
+		pc2.createPlayer();
+
+		
+		playerController.fold();
+
+		Player secondPlayer = pc2.getPlayer();
+
+		assertTrue(table.isOwner(secondPlayer));
+	}
 
 }
