a0f77ee360f08948a0e84650068d751467aec78d
Alin Pandichi <alin.pandichi@gmail.com>
2015-03-21 11:33:43 +0200

    Added rank and suit coonverters to html symbols

---
+++ src/main/java/converters/SuitToHtmlSymbol.java
@@ -0,0 +1,29 @@
+package converters;
+
+import cards.Suit;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ * Created by pndl on 3/21/15.
+ */
+@FacesConverter("converters.SuitToHtmlSymbol")
+public class SuitToHtmlSymbol implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object o) {
+        if (o.equals(Suit.CLUBS)) return "&clubs;";
+        if (o.equals(Suit.SPADES)) return "&spades;";
+        if (o.equals(Suit.HEARTS)) return "&hearts;";
+        if (o.equals(Suit.DIAMONDS)) return "&diams;";
+        return null;
+    }
+}
