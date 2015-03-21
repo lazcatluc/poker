fe9b6fc342397c0359c3fa5765f0bce81cd37bfe
ursuandrei42 <ursuandrei42-PC>
2015-03-21 10:30:28 +0200

    remeber draw cards for player

--- src/test/java/controller/CardControllerTest.java
+++ src/test/java/controller/CardControllerTest.java
@@ -5,11 +5,15 @@ import static org.mockito.Mockito.*;
 
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
 import cards.Card;
+import cards.CardImpl;
 import cards.Deck;
+import cards.Rank;
+import cards.Suit;
 
 public class CardControllerTest {
 
@@ -17,11 +21,17 @@ public class CardControllerTest {
 	
 	private CardController cardController;
 	
+	private Card card1;
+	private Card card2;
+	
 	@Before
 	public void setup(){
 		deck = mock(Deck.class);
 		
 		
+		card1 = new CardImpl(Rank.ACE,Suit.CLUBS);
+		card2 = new CardImpl(Rank.FIVE,Suit.HEARTS);
+		when(deck.drawCard()).thenReturn(card1, card2);
 		cardController = new CardController();
 		cardController.setDeck(deck);
 	}
@@ -36,12 +46,15 @@ public class CardControllerTest {
 	public void getPlayerCardsReturnsListOfTwoCards() throws Exception {
 		List<Card> playerCards = cardController.getPlayerCards();
 		
-		assertEquals(2,playerCards.size());
+		Assertions.assertThat(playerCards).hasSize(2);
 	}
 	
 	@Test
-	public void getPlayerCardsReturnDifferentListsOfCards() throws Exception {
+	public void getPlayerCardsReturnDifferentCards() throws Exception {
+		List<Card> firstDraw = cardController.getPlayerCards();
+		List<Card> secondDraw = cardController.getPlayerCards();
 		
+		Assertions.assertThat(firstDraw).isEqualTo(secondDraw);
 	}
 
 }
