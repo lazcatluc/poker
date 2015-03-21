671757ad7bd255e250c64c8b8fba2ca01c65e116
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2015-03-21 09:45:00 +0200

    Add Card interface

---
+++ src/main/java/cards/Rank.java
@@ -0,0 +1,18 @@
+package cards;
+
+public enum Rank {
+    ACE,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
